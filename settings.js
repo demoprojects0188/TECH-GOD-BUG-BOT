334f8f36500375c504409014895f6e51c642d464
demoprojects0188 <atomcyberforce@gmail.com>
2024-07-25 18:29:44 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "923287280856"
+global.ownername = "ACF"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '923287280856'  //creator number
+global.ownername = 'ACF' //owner name
+global.botname = 'ACF' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
